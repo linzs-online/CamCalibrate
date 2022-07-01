bd1dda8ebb0de49fe919a9a633651de19e1afc02
linzs.online <linzs.online@gmail.com>
2022-07-01 20:31:08 +0800

    baseline

---
+++ build/CMakeFiles/camCalibrate.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lin/SLAMcode/mySrc/camCalibrate/camCalibrate.cpp" "/home/lin/SLAMcode/mySrc/camCalibrate/build/CMakeFiles/camCalibrate.dir/camCalibrate.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lin/OpenCV_Src/opencv-4.2.0/build"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/core/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/flann/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/imgproc/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/ml/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/photo/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/dnn/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/features2d/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/gapi/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/imgcodecs/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/videoio/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/calib3d/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/highgui/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/objdetect/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/stitching/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/ts/include"
+  "/home/lin/OpenCV_Src/opencv-4.2.0/modules/video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/camCalibrate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camCalibrate.dir/camCalibrate.cpp.o"
+  "camCalibrate"
+  "camCalibrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camCalibrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
